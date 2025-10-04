982ff4410c67792a72febb4e9eabaddcf7020fc1
muhdshahan <shahuuraff@gmail.com>
2025-10-04 08:23:36 +0530

    Write segment_customers func

--- backend/ml/customer_segmentation.py
+++ backend/ml/customer_segmentation.py
@@ -11,3 +11,16 @@ async def segement_customers(db: AsyncSession, n_clusters=3):
     """
     ML-based customer segmentation using KMeans.
     """
+
+    # Step 1: Fetch user sales data
+    result = await db.execute(select(
+        Order.user_id,
+        func.count(Order.id).label("num_orders"),
+        func.sum(Order.total_price).label("total_spent"),
+        func.max(Order.created_at).label("last_order")
+        ).group_by(Order.user_id))
+    
+    user_stats = result.all()
+    
+    if not user_stats:
+        return {"msg": "No sales data for segmentation"}
